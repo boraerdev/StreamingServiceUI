e70cb46e699c8b793047d21446e509d6c44601e3
Bora Erdem <boraerdev@gmail.com>
2022-09-15 00:48:03 +0300

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Bora Erdem on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
